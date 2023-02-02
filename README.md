2f11691374593e397a2b37e52a5f2d16f312212f
jSohnData <jihos@me.com>
2023-02-02 02:21:22 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Magic Color XXL
+What started as a random hex color generator turned in a music enthralled fun experimental site.
+
+[https://magic-color-xxl.web.app/](https://magic-color-xxl.web.app/)
--- scripts/main.js
+++ scripts/main.js
@@ -2,18 +2,36 @@ function getRandomColor() {
     const hexColorCharacter = ['0','1','2','3','4','5','6','7','8','9','0', 'A','B','C','D','E','F'];
 
     let color = "#";
+    let randIndex = "0";
     for (let i=0; i<6; i++) {
-        color += hexColorCharacter[Math.floor(Math.random() * hexColorCharacter.length)];
+        randIndex = Math.floor(Math.random() * hexColorCharacter.length);
+        color += hexColorCharacter[randIndex];
     }
 
     return color;
 }
 
-function rendderRandomColor() {
-    const currentRandomColor = getRandomColor();
-    document.getElementById("htmlBody").style.backgroundColor=currentRandomColor;
-    document.getElementById("htmlH1").innerHTML = currentRandomColor;
-    console.log(currentRandomColor);
+
+
+function getRandomBackground() {
+    const totalBG = 9;
+    const randBg = `bg${Math.floor(Math.random() * totalBG)}.webp`;
+
+    return randBg
 }
 
-rendderRandomColor();
+
+
+
+function getRandomSet() {
+    const randomColor = getRandomColor();
+    const background = getRandomBackground();
+
+    document.getElementById("htmlBody").style.backgroundColor=randomColor;
+    document.getElementById("htmlH1").innerHTML = randomColor;
+    document.title=`${randomColor} 🌈 Magic Color XXL`;
+
+    document.getElementById("htmlBody").style.backgroundImage=`url('../images/${background}')`;
+
+    console.log('Hex:',randomColor,'BG:',background);
+}
